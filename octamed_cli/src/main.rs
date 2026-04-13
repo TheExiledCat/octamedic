77276360e3de667ce2131a1ca0085cd836647399
TheExiledCat <kazekage2002@hotmail.com>
2026-04-13 06:39:25 +0200

    basicparsing

--- octamed_cli/src/main.rs
+++ octamed_cli/src/main.rs
@@ -2,7 +2,6 @@ use std::{ env::{ self, ArgsOs }, path::PathBuf };
 
 use octamed::{ mmd0::parser::OctamedMMD0Parser, utility::logger::ConsoleLogger };
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = PathBuf::from(args.get(1).unwrap());
-    OctamedMMD0Parser::parse_file(path.as_path(), ConsoleLogger::new()).unwrap()
+    let path = PathBuf::from("example_meds/daft_punk.med");
+    let res = OctamedMMD0Parser::parse_file(path.as_path(), ConsoleLogger::new()).unwrap();
 }
