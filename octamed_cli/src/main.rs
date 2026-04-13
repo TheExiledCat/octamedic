a0429a775705666ce1bbd5bcd65dfa28359cd368
TheExiledCat <kazekage2002@hotmail.com>
2026-04-13 18:17:56 +0200

    mmd

--- octamed_cli/src/main.rs
+++ octamed_cli/src/main.rs
@@ -2,6 +2,6 @@ use std::{ env::{ self, ArgsOs }, path::PathBuf };
 
 use octamed::{ mmd0::parser::OctamedMMD0Parser, utility::logger::ConsoleLogger };
 fn main() {
-    let path = PathBuf::from("example_meds/daft_punk.med");
+    let path = PathBuf::from("example_meds/example.mmd0");
     let res = OctamedMMD0Parser::parse_file(path.as_path(), ConsoleLogger::new()).unwrap();
 }
