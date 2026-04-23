2a5961701abf3bf1b41cc26c76d6485d48cd70ac
TheExiledCat <kazekage2002@hotmail.com>
2026-04-23 19:51:09 +0200

    font

--- octamedic_gui/src/app.rs
+++ octamedic_gui/src/app.rs
@@ -1,12 +1,16 @@
 use pix_engine::{
     color::Color,
     engine::PixEngine,
+    image::PixelFormat,
     point,
     prelude::{ Font, RectMode },
     rect,
     shape::Rect,
+    state::PixState,
     vector,
 };
+
+const TOPAZ_BYTES: &[u8] = include_bytes!("../assets/fonts/amiga-topaz.ttf");
 
 pub struct App {
     block: Rect,
@@ -29,6 +33,7 @@ impl PixEngine for App {
         &mut self,
         s: &mut pix_engine::prelude::PixState
     ) -> pix_engine::prelude::PixResult<()> {
+        s.font_family(Font::from_bytes("amiga-topaz", TOPAZ_BYTES))?;
         return Ok(());
     }
     fn on_update(
@@ -36,17 +41,14 @@ impl PixEngine for App {
         s: &mut pix_engine::prelude::PixState
     ) -> pix_engine::prelude::PixResult<()> {
         s.background(Color::BLACK);
-        s.font_family(Font::NOTO)?;
-        s.font_size(16)?;
         s.clip(None)?;
         s.rect_mode(RectMode::Corner);
+
         let (w, h) = s.display_dimensions()?;
 
         let scale_x = (w as f32) / (self.size.x() as f32);
         let scale_y = (w as f32) / (self.size.y() as f32);
-        s.set_window_dimensions((w, h))?;
-        s.set_cursor_pos(point!(0, 0));
-        s.scale(scale_x, scale_y)?;
+
         s.fill(Color::PINK);
         let w = self.size.x();
         let h = self.size.y();
@@ -55,9 +57,19 @@ impl PixEngine for App {
 
         let pos = s.mouse_pos();
         s.fill(Color::RED);
+        s.font_size(2 * (scale_x.max(scale_y) as u32))?;
+
         s.text(format!("w: {}, h: {}, mx: {}, my: {}", w, h, pos.x(), pos.y()))?;
+        s.line([
+            [0, 0],
+            [self.size.x() as i32, self.size.y() as i32],
+        ])?;
         s.fill(Color::WHITE);
         self.place(pos.x(), pos.y());
         return s.rect(self.block);
     }
+}
+
+trait TextPoint {
+    fn text_point<S>(&mut self, text: S) -> pix_engine::prelude::PixResult<()> where S: AsRef<str>;
 }
